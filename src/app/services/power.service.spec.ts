08516d2ef6494082dda117e849c7ae512ab6d68d
Antonio Angosto <aangosto@disid.com>
2021-02-26 11:42:33 +0100

    test: fix tests

--- src/app/services/power.service.spec.ts
+++ src/app/services/power.service.spec.ts
@@ -19,6 +19,11 @@ describe('PowerService', () => {
     expect(response).toBe(4);
   });
 
+  it('should return not be 0 when base is 3 and exponent is 3', () => {
+    const response = service.power(3, 3);
+    expect(response).not.toBe(0);
+  });
+
   it('should return 8 when base is 2 and exponent is 3', () => {
     const response = service.power(2, 3);
     expect(response).toBe(8);
@@ -27,11 +32,6 @@ describe('PowerService', () => {
   it('should return 8 when base is 3 and exponent is 2', () => {
     const response = service.power(3, 2);
     expect(response).toBe(8);
-  });
-
-  it('should return not be 0 when base is 3 and exponent is 3', () => {
-    const response = service.power(3, 3);
-    expect(response).not.toBe(0);
   });
 
   it('should return 27 when base is 3 and exponent is 3', () => {
