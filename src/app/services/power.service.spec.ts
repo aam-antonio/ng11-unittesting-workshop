6b2a65b8ec9794ac800d9631f93b4469d18fb00a
Antonio Angosto <aangosto@disid.com>
2021-02-26 11:27:58 +0100

    test: add tests

--- src/app/services/power.service.spec.ts
+++ src/app/services/power.service.spec.ts
@@ -13,4 +13,24 @@ describe('PowerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return 4 when base is 2 and exponent is 2', () => {
+    const response = service.power(2, 2);
+    expect(response).toBe(4);
+  });
+
+  it('should return NaN when base is 2 and exponent is not 2', () => {
+    const response = service.power(2, 3);
+    expect(response).toBeNaN();
+  });
+
+  it('should return NaN when base is not 2 and exponent is 2', () => {
+    const response = service.power(3, 2);
+    expect(response).toBeNaN();
+  });
+
+  it('should return NaN when base is not 2 and exponent is not 2', () => {
+    const response = service.power(3, 3);
+    expect(response).toBeNaN();
+  });
 });
